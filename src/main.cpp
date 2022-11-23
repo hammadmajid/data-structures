3628e372ab5ff41ceccd2a82f439abad65d4c539
hammadmajid <hm.hammadmajid@gmail.com>
2022-11-23 18:09:39 +0500

    Document struct node

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,13 @@
 #include <iostream>
 #include <stdlib.h>
 
+/// @brief In a linked list each is connected by node
+///         node contains two field `data` and `next`
 struct node
 {
+    /// @brief stores the actual data
     int data;
+    /// @brief points to next node
     node *next;
 };
 int main(int argc, char const *argv[])
